b9881bf69050de644280f245d56227e022f55b6e
s3a317097 <s3a317097@users.noreply.github.com>
2018-01-11 13:05:05 +0800

    將錯誤訊息印在 View 上

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -23,12 +23,15 @@
         <div class="alert alert-danger alert-dismissable">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
             <i class="fa fa-info-circle"></i>  <strong>警告！</strong> 請修正表單錯誤：
+            @foreach($errors->all() as $error)
+                <li>{{$error}}</li>
+            @endforeach
         </div>
     </div>
 </div>
 @endif
 <!-- /.row -->
-
+@include('admin.layouts.partials.validation')
 <div class="row">
     <div class="col-lg-12">
         <form action="/admin/posts" method="POST" role="form">
